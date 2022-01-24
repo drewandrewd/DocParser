98ae71e1c8e8abc6dd0c34660dd9741ef258aab0
drewandrewd <andrewid10@gmail.com>
2022-01-24 22:35:38 +0500

    new model

---
+++ src/main/java/com/example/program/models/Site.java
@@ -0,0 +1,11 @@
+package com.example.program.models;
+
+import java.util.Map;
+
+public class Site {
+
+    private String id;
+
+    private String site;
+    private Map<String, Integer> words;
+}
